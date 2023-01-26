2104ea7be4909d049ba27fe37a5d4d1799b00bfc
Ilona Mereuta <edoilo.3521@gmail.com>
2023-01-26 18:21:05 +0100

    banner product page

--- app/src/Venduti.js
+++ app/src/Venduti.js
@@ -1,13 +1,21 @@
+import { Box, Typography } from "@mui/material";
 import React from "react";
 import { CheckboxProduct } from "./checkbox/CheckboxProduct";
 import ProductsList from "./Products/DataProducts";
+import "./checkbox/productPage.css"
 
 export default function Venduti() {
   return (
-    <div>
-      La pagina dei più Venduti
+    <Box className="product-page">
+      <Typography className="banner-black" variant="h4">I più venduti</Typography>
+      <Box className="banner-box">
+      <Typography variant="h6">I set LEGO® più popolari</Typography>
+      <p>Benvenuto nell'elenco definitivo dei set LEGO più popolari in questo momento. Da complessi articoli da collezione per gli adulti ad avventurosi set per i bambini, i costruttori di tutte le età adorano questi set. I set LEGO più venduti sono perfetti da donare alle persone che ami o per regalarti un nuovo set. Questo elenco viene aggiornato regolarmente, per consentirti di trovare sempre i set e i giocattoli più popolari. Cerchi ancora più ispirazione? Dai un'occhiata ai set in arrivo, disponibili solo su LEGO.com e negli Store o risparmia con una delle ultime offerte.
+      </p>
+      </Box>
+
       <CheckboxProduct />
       <ProductsList />
-    </div>
+    </Box>
   );
 }
