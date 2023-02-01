d2fafcb71fd332c83566fb45fbf1030688ffc174
Ilona Mereuta <edoilo.3521@gmail.com>
2023-02-01 10:53:55 +0100

    responsive button bunner, grid

--- app/src/Banner/Button/BannerButtonDark.js
+++ app/src/Banner/Button/BannerButtonDark.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import Stack from "@mui/material/Stack";
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import { Button } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import "./style.css";
 
 
@@ -10,8 +10,9 @@ export default function UnstyledButtonsSimple() {
   return (
           <div className="bannerButton">
             <Stack spacing={2} direction="row">
-              <Button className="btnMedia" style={{ display: "flex", alignItems: "center", gap: "5px", color: "black", backgroundColor: "white", marginTop: 10, fontSize: {xs: 10, sm: 12, md: 14, lg: 28, xl: 30 }}}>
-                Compra ora {<ArrowForwardIosIcon style={{ width: {sx: "10px", sm: "12px", md: "20px", lg: "55px", xl: "30px"} }} />}
+              <Button variant="contained" endIcon={<ArrowForwardIosIcon />} className="btnMedia" style={{ color: "black", backgroundColor: "white", height: "35px",  marginTop: 10, fontSize: {xs: "8px", sm: "8px", md: "14px"}}}>
+                <Typography sx={{fontSize: {xs:10, sm: 14, md: 16, lg: 20, xl: 22 } }}> Compra ora</Typography>
+
               </Button>
              </Stack>
             </div>
--- app/src/Banner/Button/BannerButtonLight.js
+++ app/src/Banner/Button/BannerButtonLight.js
@@ -11,8 +11,8 @@ export default function UnstyledButtonsSimple() {
   return (
           <div className="bannerButton">
             <Stack spacing={2} direction="row">
-              <Button className="btnMedia" style={{ display: "flex", alignItems: "center", gap: "5px", color: "black", backgroundColor: "white", marginTop: 10, fontSize: {xs: 10, sm: 12, md: 14} }}>
-                Compra ora {<ArrowForwardIosIcon style={{ width: {sx: "10px", sm: "12px", md: "20px", lg: "55px", xl: "30px"} }} />}
+              <Button className="btnMedia" style={{ alignItems: "center", color: "black", backgroundColor: "white", height: "20px",  marginTop: 10, fontSize: {xs: "px", sm: 12, md: 14} }}>
+                Buy Now {<ArrowForwardIosIcon style={{ width: {sx: "10px", sm: "12px", md: "20px", lg: "55px", xl: "30px"} }} />}
               </Button>
              </Stack>
             </div>
